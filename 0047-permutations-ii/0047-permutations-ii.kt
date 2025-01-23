c5b93414465d68c88f351a1040fae1dc1224fe71
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2025-01-23 19:07:33 +0900

    Time: 27 ms (51.3%), Space: 41.6 MB (75.65%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.kt
+++ 0047-permutations-ii/0047-permutations-ii.kt
@@ -1,34 +1,39 @@
 class Solution {
-    
-    val answer = mutableSetOf<List<Int>>()
+
+    val answer = mutableListOf<List<Int>>()
     lateinit var src: IntArray
     lateinit var tgt: IntArray
     lateinit var selected: BooleanArray
-    
-    fun permuteUnique(nums: IntArray): List<List<Int>> {
 
+    fun permuteUnique(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        
         src = nums
         tgt = IntArray(nums.size)
         selected = BooleanArray(nums.size)
-        
+
         recur(0)
-        
-        return answer.toList()
+
+        return answer
     }
-    
+
     fun recur(depth: Int) {
         if (depth == src.size) {
             answer.add(tgt.toList())
             return
         }
-        
+
+        var lastSelectedNum = 100
+
         src.forEachIndexed { index, num ->
-            if (!selected[index]) {
+            if (!selected[index] && lastSelectedNum != num) {
                 selected[index] = true
                 tgt[depth] = num
-                
+
                 recur(depth + 1)
+
                 selected[index] = false
+                lastSelectedNum = num
             }
         }
     }
