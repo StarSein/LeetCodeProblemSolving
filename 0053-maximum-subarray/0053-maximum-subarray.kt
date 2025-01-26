d503d782e2fcde21e0a30a3ae41da7cef3e7fdb1
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2025-01-26 17:19:48 +0900

    Time: 66 ms (5.06%), Space: 56 MB (52.28%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.kt
+++ 0053-maximum-subarray/0053-maximum-subarray.kt
@@ -2,32 +2,63 @@ import kotlin.math.max
 
 /**
  *
- * 1. Iterate the array left to right.
- * 2. As soon as a subarray with range[s:e] of which sum is negative,
- *    remove the range.
- * 3. Because there is no `i` that makes range[i:e]'s sum is bigger than range[s:e] ( i < s ),
- *    the removal is logically consistent to find the optimal range.
+ * Manage a segment's state with four properties
+ * to find the max value of sum of the subarray.
+ *  1) leftMaxSum: the max value of sum of the subarray which contains the leftmost element.
+ *  2) rightMaxSum: the max value of sum of the subarray which contains the rightmost element.
+ *  3) entireSum: the sum of the segment
+ *  4) maxSum: the max value of sum of the subarray in the segment
  *
- * Time Complexity: O(N)
+ * Time Complexity: O(NlogN)
  * Space Complexity: O(N)
+ *
  */
 
 class Solution {
+
+    class Segment(
+        val leftMaxSum: Int,
+        val rightMaxSum: Int,
+        val entireSum: Int,
+        val maxSum: Int
+    )
+
     fun maxSubArray(nums: IntArray): Int {
-        if (nums.all { it < 0 }) {
-            return nums.max()
+        return dnc(nums, 0, nums.lastIndex).maxSum
+    }
+
+    fun dnc(nums: IntArray, startIndex: Int, lastIndex: Int): Segment {
+        if (startIndex == lastIndex) {
+            val num = nums[startIndex]
+            return Segment(
+                leftMaxSum = num,
+                rightMaxSum = num,
+                entireSum = num,
+                maxSum = num
+            )
         }
-        
-        var answer = 0
-        var curSum = 0
-        nums.forEach { num ->
-            if (curSum + num < 0) {
-                curSum = 0
-            } else {
-                curSum += num
-            }
-            answer = max(answer, curSum)
-        }
-        return answer
+
+        val midIndex = (startIndex + lastIndex) / 2
+
+        val leftSeg = dnc(nums, startIndex, midIndex)
+        val rightSeg = dnc(nums, midIndex + 1, lastIndex)
+
+        val leftMaxSum = max(leftSeg.leftMaxSum, leftSeg.entireSum + rightSeg.leftMaxSum)
+        val rightMaxSum = max(rightSeg.rightMaxSum, leftSeg.rightMaxSum + rightSeg.entireSum)
+        val entireSum = leftSeg.entireSum + rightSeg.entireSum
+        val maxSum = arrayOf(
+            leftSeg.maxSum,
+            rightSeg.maxSum,
+            leftMaxSum,
+            rightMaxSum,
+            leftSeg.rightMaxSum + rightSeg.leftMaxSum
+        ).max()
+
+        return Segment(
+            leftMaxSum = leftMaxSum,
+            rightMaxSum = rightMaxSum,
+            entireSum = entireSum,
+            maxSum = maxSum
+        )
     }
 }
